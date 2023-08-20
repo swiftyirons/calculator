3827ef5722fd24ac1bf23130d77689b0b919ae44
swiftyirons <135535040+swiftyirons@users.noreply.github.com>
2023-08-19 19:00:53 -0500

    Add operate function

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
 function add(x, y) {
  return x + y;
 }
+
 function subtract(x, y) {
  return x - y;
 }
+
 function multiply(x, y) {
   return x * y;
 }
+
 function divide(x,y) {
   return x  / y;
 }
+
+function operate(operator, a, b) {
+  switch(operator) {
+    case "+":
+      solution = add(a, b);
+      break;
+    case "-":
+      solution = subtract(a, b);
+      break; 
+    case "*":
+      solution = multiply(a, b);
+      break;
+    case "/":
+      solution = divide(a, b);
+      break;
+  }
+  return solution;
+}
+
+// console.log(operate("+", 8, 4));
